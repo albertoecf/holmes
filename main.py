4e1de9572b171902191572a8926a007d877589d0
bertek <81099969+albertoecf@users.noreply.github.com>
2024-01-15 07:43:17 +0100

    feat add LCEL and user input

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+import click
 from langchain.llms import Ollama
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.output_parser import StrOutputParser
 
-ollama = Ollama(base_url="http://localhost:11434", model="llama2")
-print(ollama("Who was Albert Einstein?"))
+
+@click.command()
+@click.option(
+    "--person_name",
+    prompt="Enter the name of the person",
+    default="Albert Einstein",
+    help="Name of the person",
+)
+def main(person_name):
+    prompt_template = ChatPromptTemplate.from_template(
+        "Provide a short description about {person}"
+    )
+    model = Ollama(base_url="http://localhost:11434", model="llama2")
+    chain = prompt_template | model
+
+    result = chain.invoke({"person": person_name})
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
